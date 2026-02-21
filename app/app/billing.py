ec98bfa5d1f495e8b9ddcd2b5e62f6af7f2b4520
mpraveen2302-dotcom <mpraveen2302@gmail.com>
2026-02-21 12:13:44 +0530

    Create billing.py

---
+++ app/app/billing.py
@@ -0,0 +1,17 @@
+import stripe
+import os
+
+stripe.api_key = os.getenv("STRIPE_SECRET")
+
+def create_checkout(price_id):
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price': price_id,
+            'quantity': 1,
+        }],
+        mode='subscription',
+        success_url='https://yourapp.com/success',
+        cancel_url='https://yourapp.com/cancel',
+    )
+    return session.url
