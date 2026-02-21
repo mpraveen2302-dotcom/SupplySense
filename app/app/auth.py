6a5a79e9719eb35b7925c16576429b6541ec292b
mpraveen2302-dotcom <mpraveen2302@gmail.com>
2026-02-21 12:09:48 +0530

    Create auth.py

---
+++ app/app/auth.py
@@ -0,0 +1,18 @@
+import jwt
+import datetime
+import os
+
+SECRET = os.getenv("JWT_SECRET")
+
+def generate_token(username):
+    payload = {
+        "user": username,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=8)
+    }
+    return jwt.encode(payload, SECRET, algorithm="HS256")
+
+def verify_token(token):
+    try:
+        return jwt.decode(token, SECRET, algorithms=["HS256"])
+    except:
+        return None
